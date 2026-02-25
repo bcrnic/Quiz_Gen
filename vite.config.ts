36e46942c81287c950c8cd69e0a87ee60215624b
Bojan Crnic <bojan.crnic@checkitlabs.com>
2026-02-25 09:32:31 +0100

    Fix base path for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Quiz_Gen/" : "/",
+  base: "/",
   server: {
     host: "127.0.0.1",
     port: 5173,
