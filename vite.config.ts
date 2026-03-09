63e2506d6fdddd3ae7e0f92ecf66d938db1dabd0
bc <bojancrnic@yahoo.com>
2026-03-09 22:22:01 +0100

    chore: revert to capitalized base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/quiz_gen/",
+  base: "/Quiz_Gen/",
   server: {
     host: "127.0.0.1",
     port: 5173,
